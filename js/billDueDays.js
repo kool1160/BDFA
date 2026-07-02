d17e8d536a53ec9fb5bec9904560bb087630e4e0
Christopher Hilton <kool1160@gmail.com>
2026-07-02 15:13:18 -0400

    Emit source data event for due-day bill saves

--- js/billDueDays.js
+++ js/billDueDays.js
@@ -206,6 +206,7 @@ function handleBillSubmitWithDueDay(event) {
   saveRows(billStorageKey, data.bills);
   resetBillForm();
   renderBillsDashboard();
+  dispatchSourceDataUpdated();
 }
 
 function syncBillDueDayEditValue(event) {
